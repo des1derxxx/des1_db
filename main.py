842a43c3456855fb0feea88604ebbdf80ceaf101
des1derx <ralo.ivan3002@gmail.com>
2022-09-04 18:02:08 +0300

    add123

--- main.py
+++ main.py
@@ -13,18 +13,49 @@ logger.setLevel(logging.DEBUG)
 db_connection = psycopg2.connect(DB_URI, sslmode="require")
 db_object = db_connection.cursor()
 
+
+def update_messages_count(user_id):
+    db_object.execute(f"UPDATE user SET messages = messages + 1 WHERE id = {user_id}")
+    db_connection.commit()
+
+
 @bot.message_handler(commands=["start"])
 def start(message):
     user_id = message.from_user.id
     username = message.from_user.username
     bot.reply_to(message, f"Hello, {username}!")
 
-    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    db_object.execute(f"SELECT id FROM user WHERE id = {user_id}")
     result = db_object.fetchone()
 
     if not result:
-        db_object.execute("INSERT INTO users(id, username) VALUES (%s, %s, %s)", (user_id, username))
+        db_object.execute("INSERT INTO user(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
         db_connection.commit()
+
+    update_messages_count(user_id)
+
+
+@bot.message_handler(commands=["stats"])
+def get_stats(message):
+    db_object.execute("SELECT * FROM user ORDER BY messages DESC LIMIT 10")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = "- Top flooders:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
+    user_id = message.from_user.id
+    update_messages_count(user_id)
+
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
