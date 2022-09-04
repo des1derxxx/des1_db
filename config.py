ae0dec28768bdb0f168c0ab70d45252d9a2ce663
des1derx <ralo.ivan3002@gmail.com>
2022-09-04 16:56:52 +0300

    new

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "5388918585:AAFg0ekwqHvuARAEVWlFirkKWQiZiGeDZsU"
+APP_URL = "https://desbotdb.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://zzypeaqivxxzkm:93efa08d12a87ad37e21e8028c7d423439e134ca023aa0b0a7e0199ea809cfae@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/deuhovrdana8v3"
